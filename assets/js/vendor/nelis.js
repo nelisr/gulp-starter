c9a5b1df7abdad3aa9a0689640a56524ec17054b
Nelis Rodrigues <nelis@e-deas.com.br>
2016-09-27 15:14:04 -0300

    ajustes no arquivo gulp.js

---
+++ assets/js/vendor/nelis.js
@@ -0,0 +1,3 @@
+$(document).ready(function($){
+    alert('Deu certo de novo!!');
+});
--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,6 +3,7 @@
 	var gulp = require( 'gulp' ),
 		uglify = require( 'gulp-uglify' ),
 		sass = require( 'gulp-sass' ),
+		concat = require('gulp-concat'),
 		cssmin = require( 'gulp-cssmin' ),
 		rename = require( 'gulp-rename' ),
 		watch = require( 'gulp-watch' ),
@@ -11,48 +12,44 @@
 		imageminJpegtran = require('imagemin-jpegtran'),
 		imageminOptipng = require('imagemin-optipng');
 
+	// Compilar sass em css e mimifica
+	gulp.task('sass', function () {
+		gulp.src(['../assets/scss/*.scss','../assets/scss/**/*.scss'])
+			.pipe(sass.sync().on('error', sass.logError))
+			.pipe(concat('style.min.css'))
+			.pipe(cssmin())
+			.pipe(gulp.dest('../build/css'));
+	});
 
 	// Mimifica arquivos JS e joga para dentro de 'build/js'
 	gulp.task('jsmin', function() {
-			gulp.src('../assets/js/main.js')
+			gulp.src(['../assets/js/main.js','../assets/js/**/*.js'])
+			   .pipe(concat('main.min.js'))
 	           .pipe(uglify())
-	           .pipe(rename({suffix: '.min'}))
 	           .pipe(gulp.dest('../build/js')); 
-	});
-
-	// Compilar sass em css
-	gulp.task('sass', function () { 
-	    gulp.src('../assets/scss/style.scss') 
-	        .pipe(sass()) 
-	        .pipe(gulp.dest('../build/css/')); 
-	});
-
-	// Mimifica o CSS compilado
-	gulp.task('cssmin', function () {
-	    gulp.src('../build/css/style.css')
-	        .pipe(cssmin())
-	        .pipe(rename({suffix: '.min'}))
-	        .pipe(gulp.dest('../build/css/'));
 	});
 
 	// Comprime imagens
 	gulp.task('imagecompress', function(){
-		gulp.src( '../assets/images/*.{png,jpg,gif}' )
+		gulp.src( ['../assets/images/*.{png,jpg,gif}', '../assets/images/**/*.{png,jpg,gif}' ])
 		.pipe( imagemin({
 			optimizationLevel: 7,
 			progressive: true
 		}))
-		.pipe( gulp.dest( '../build/images/' ) );
+		.pipe(gulp.dest('../build/images'));
 	});
 
 	// Observa os arquivos para executar as tarefas
 	gulp.task('watch', function() {
 	  gulp.watch('../assets/js/main.js',['jsmin']);
+	  gulp.watch('../assets/js/**/*.js',['jsmin']);
 	  gulp.watch('../assets/scss/style.scss',['sass']);
-	  gulp.watch('../build/css/style.css',['cssmin']); 
-	}); 
+	  gulp.watch('../assets/scss/**/*.scss',['sass']);
+	  gulp.watch('../assets/images/*.{png,jpg,gif}',['imagecompress']);
+	  gulp.watch('../assets/images/**/*.{png,jpg,gif}',['imagecompress']);
+	});
 	
 	// Tarefa padrão 
-	gulp.task('default', ['watch','jsmin','sass','cssmin','imagecompress']);
+	gulp.task('default', ['watch','jsmin','sass','imagecompress']);
 	    
 	   
