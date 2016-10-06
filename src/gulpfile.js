4ec2f336e2f8f207319d6d14f5351a323f6ab3e4
Nelis Rodrigues <nelis@e-deas.com.br>
2016-10-06 09:05:03 -0300

    ajustes no arquivo de configuração

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -7,7 +7,6 @@
 		cssmin = require( 'gulp-cssmin' ),
 		watch = require( 'gulp-watch' ),
 		imagemin = require( 'gulp-imagemin' ),
-		clean = require('gulp-clean'),
 		cleanDest = require('gulp-clean-dest');
 
 	// Compile sass to css and mimify
@@ -23,8 +22,9 @@
 	gulp.task('jsmin', function() {
 			gulp.src(['../assets/js/main.js','../assets/js/**/*.js'])
 			   .pipe(concat('main.min.js'))
-	           .pipe(uglify())
-	           .pipe(gulp.dest('../build/js'));
+	       .pipe(uglify())
+				 .on('error', console.error.bind(console))
+	       .pipe(gulp.dest('../build/js'));
 	});
 
 	// Comprime imagens
@@ -34,6 +34,7 @@
 			optimizationLevel: 7,
 			progressive: true
 		}))
+		.on('error', console.error.bind(console))
 		.pipe(cleanDest('../build/images'))
 		.pipe(gulp.dest('../build/images'));
 	});
@@ -43,46 +44,20 @@
 	gulp.task('fonts',  function(){
 		return gulp.src('../assets/fonts/*.{eot,svg,ttf,woff,woff2}')
 		.pipe(cleanDest('../build/fonts'))
+		.on('error', console.error.bind(console))
     .pipe(gulp.dest('../build/fonts'));
 	});
 
 	// Observa os arquivos para executar as tarefas
 	gulp.task('watch', function() {
 
-		gulp.watch('../assets/js/main.js',['jsmin']);
-		gulp.watch('../assets/js/main.js', function (event) {
-    	console.log(event);
-  	});
+		gulp.watch(['../assets/js/main.js','../assets/js/**/*.js'],['jsmin']);
 
-	  gulp.watch('../assets/js/**/*.js',['jsmin']);
-		gulp.watch('../assets/js/**/*.js', function (event) {
-    	console.log(event);
-  	});
+	  gulp.watch([]'../assets/scss/style.scss', '../assets/scss/**/*.scss'],['sass']);
 
-	  gulp.watch('../assets/scss/style.scss',['sass']);
-		gulp.watch('../assets/scss/style.scss', function (event) {
-    	console.log(event);
-  	});
-
-	  gulp.watch('../assets/scss/**/*.scss',['sass']);
-		gulp.watch('../assets/scss/**/*.scss', function (event) {
-    	console.log(event);
-  	});
-
-		gulp.watch('../assets/images/*.{png,jpg,gif}',['imagecompress']);
-		gulp.watch('../assets/images/*.{png,jpg,gif}', function (event) {
-    	console.log(event);
-  	});
-
-		gulp.watch('../assets/images/**/*.{png,jpg,gif}',['imagecompress']);
-		gulp.watch('../assets/images/**/*.{png,jpg,gif}', function (event) {
-    	console.log(event);
-  	});
+		gulp.watch(['../assets/images/*.{png,jpg,gif}','../assets/images/**/*.{png,jpg,gif}'],['imagecompress']);
 
 		gulp.watch('../assets/fonts/*.{eot,svg,ttf,woff,woff2}', ['fonts']);
-		gulp.watch('../assets/fonts/*.{eot,svg,ttf,woff,woff2}', function (event) {
-    	console.log(event);
-  	});
 
 	});
 
